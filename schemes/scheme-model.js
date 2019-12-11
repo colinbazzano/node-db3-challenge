f651f7e396a8845dd698b317c28c62602f41bb7f
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-11 16:23:28 -0500

    Created a db config file for Knex and created the find and findById functions

---
+++ schemes/scheme-model.js
@@ -0,0 +1,18 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById
+  //   findSteps,
+  //   add,
+  //   update,
+  //   remove
+};
+
+function find() {
+  return db("schemes");
+}
+
+function findById(id) {
+  return db("schemes").where({ id });
+}
